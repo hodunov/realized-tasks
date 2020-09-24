62f95becc8dc33d4e8e49cfb6b4571900dec9701
hodunov <hodunov.art@gmail.com>
2020-09-24 22:46:58 +0300

    return the highest and lowest number in str

--- codewars.py
+++ codewars.py
@@ -10,8 +10,7 @@ high_and_low("1 9 3 4 -5") # return "9 -5"
 
 def high_and_low(numbers):
     numbers = [int(each) for each in numbers.split()]
-    print(numbers)
-    result = f"{max(numbers)} {min(numbers)}"
-    return result
+    return f"{max(numbers)} {min(numbers)}"
 
-print(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"))
+print(high_and_low("4 5 29 54 4 0 -214 544 -64 1 -3 6 -6"))
+print(high_and_low("1 2 3 4 5"))
