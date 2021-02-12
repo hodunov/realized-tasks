620820ebc9a80e24e8b8c4f8a926afedfeb450dd
hodunov <hodunov.art@gmail.com>
2021-02-12 18:19:57 +0200

    Triple Trouble 2

--- codewars.py
+++ codewars.py
@@ -307,7 +307,8 @@ def trouble(lst, target):
 
 """
 Triple Trouble
-Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. 
+Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
 
 E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
 
@@ -316,7 +317,7 @@ Note: You can expect all of the inputs to be the same length.
 """
 
 def triple_trouble(one, two, three):
-    return ''.join(map(str, [one[i]+two[i]+three[i] for i in range(len(one))]))
+    return ''.join([one[i]+two[i]+three[i] for i in range(len(one))])
 
 
 print(triple_trouble("Bm", "aa", "tn")) # should equal "Batman"
