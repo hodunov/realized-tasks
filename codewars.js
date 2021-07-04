8ffa5b049a932474a4367527866e2c962dd04688
Artem Hodunov <hodunov.art@gmail.com>
2021-07-04 16:21:58 +0300

    Convert the equivalent binary value to an integer

--- codewars.js
+++ codewars.js
@@ -3,12 +3,19 @@
 // https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
 
 function filter_list(l) {
-  // Return a new array with the strings filtered out
-  var result = [];
-  for (var item of l) {
-    if (typeof item === "number") {
-      result.push(item);
+    // Return a new array with the strings filtered out
+    var result = []
+    for (var item of l) {
+        if (typeof (item) === 'number') {
+            result.push(item)
+        }
     }
-  }
-  return result;
+    return result
 }
+
+// https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
+
+const binaryArrayToNumber = arr => {
+    // Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+    return parseInt(arr.join(''), 2);
+};
